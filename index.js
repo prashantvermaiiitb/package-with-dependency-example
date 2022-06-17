d48feded888f3de5d47f0f349634303b5c40bde7
Prashant Verma <prashantvermaiiitb@gmail.com>
2022-06-17 16:03:45 +0530

    usage of the package.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { add, logger } = require('sample-mathlib');
 const scopeMathlib = require('@prashantvermaiiitb/mathlib');
 
+scopeMathlib.hello();
+
 module.exports = {
     add, logger, scopeMathlib
 }
